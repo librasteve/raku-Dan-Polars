600905b8272e3a242552b816014a50a98e26d902
p6steve <p6steve@furnival.net>
2022-05-21 06:29:26 +0000

    spike3 dead end

---
+++ spike3/add_client.rakuOLD
@@ -0,0 +1,7 @@
+use NativeCall; 
+
+#cp add_dll/src/libadd.so /usr/lib (or use path)
+
+sub add (int32, int32) returns int32 is native('add') { * }; 
+
+say add(1, 2);
---
+++ spike3/add_dll/src/add.rs
@@ -0,0 +1,33 @@
+#![crate_type = "dylib"]
+use std::ffi::*;
+//use std::ffi::c_void;
+//use std::os::raw::c_void;
+ 
+// compile with: rustc add.rs
+ 
+#[no_mangle]
+pub extern "C" fn add(a:i32, b:i32) -> i32 {
+    a+b
+}
+
+//#viz. https://metacpan.org/pod/FFI::Platypus::Lang::Rust
+//#a "thin C layer"
+//
+//type CFoo = c_void;
+//
+//#[no_mangle]
+//pub extern "C" fn foo_new(_class *const i8) -> *mut CFoo {
+//    Box::into_raw(Box::new(Foo::new())) as *mut CFoo
+//}
+//
+//#[no_mangle]
+//pub extern "C" fn foo_method1(f: *mut CFoo) {
+//    let f = unsafe { &*(f as *mut Foo) };
+//    f.method1();
+//}
+//
+//#[allow(non_snake_case)]
+//#[no_mangle]
+//pub extern "C" fn foo_DESTROY(f: *mut CFoo) {
+//    unsafe { drop(Box::from_raw(f as *mut Foo)) };
+//}

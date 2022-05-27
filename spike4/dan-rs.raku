8235b64dfda097a534659e4ce820a65440c833ec
p6steve <p6steve@furnival.net>
2022-05-27 14:13:43 +0000

    working str_in / str_out

--- spike4/dan-rs.raku
+++ spike4/dan-rs.raku
@@ -2,8 +2,12 @@ use NativeCall;
 
 constant $n-path = './dan/target/debug/dan';
 
+sub str2rust(Str is encoded('utf8')) is native($n-path) {*};
+str2rust("/root/raku-Dan-Polars/spike2/pl_so/src/iris.csv");
+
+sub str2raku() returns Pointer[Str] is native($n-path) {*};
+say str2raku.deref;
+
 sub add (int32, int32) returns int32 is native($n-path) { * };  
 say add(1, 2); 
 
-sub xxx(Str is encoded('utf8')) is native($n-path) {*};
-xxx("/root/raku-Dan-Polars/spike2/pl_so/src/iris.csv");
--- spike4/dan/src/lib.rs
+++ spike4/dan/src/lib.rs
@@ -1,55 +1,36 @@
-//#![crate_type = "dylib"] #define dylib in Cargo.toml 
+use std::ffi::*; //{CStr, CString,}
+use std::os::raw::c_char;
+use std::cell::RefCell;
 
-use std::ffi::*;//{CStr}
-
-use polars::prelude::*;//{CsvReader, DataType, Field, Result as PolarResult, Schema, DataFrame,};
-use polars::prelude::{Result as PolarResult};
-use polars::frame::DataFrame;
-use std::fs::File;
-use std::path::{Path};
-//// an eye on traits:
-use polars::prelude::SerReader;
-//// If we don't import this one we might get the error CsvReader::new(file) new function not found in CsvReader
-
-//pub fn read_csv<P: AsRef<Path>>(path: P) -> PolarResult<DataFrame> {
-//    /* Example function to create a dataframe from an input csv file*/
-//    let file = File::open(path).expect("Cannot open file.");
-//
-//    CsvReader::new(file)
-//    .has_header(true)
-//    .finish()
-//}
-
-//iamerejh ... get from CStr to AsRef<Path> maybe via AsRef<OsStr>?
-pub fn read_csv(spath:&str) -> PolarResult<DataFrame> {
-    println!{"{}", spath};
-    let fpath = Path::new("/root/raku-Dan-Polars/spike2/pl_so/src/iris.csv");
-    //let fpath = Path::new(spath);
-    let file = File::open(fpath).expect("Cannot open file.");
-
-    CsvReader::new(file)
-    .has_header(true)
-    .finish()
+fn str_in(i_string: *const c_char) -> String {
+    unsafe {
+        CStr::from_ptr(i_string).to_string_lossy().into_owned()
+    }
 }
-
 #[no_mangle]
-pub extern "C" fn xxx(cfile:&CStr) {
-//fn main() -> Result<()> {
-
-  //let ifile = "/root/raku-Dan-Polars/spike2/pl_so/src/iris.csv";
-  let sfile = "some text";
-  //let sfile = cfile.to_str().unwrap();
-  println!("{}", sfile.len());
-  //let ifile = sfile;
-  let df = read_csv(sfile).unwrap();
-  println!{"{}", df.head(Some(5))};
-
-  //Ok(())  
-  //#may need some incantation like this for "no panic" return types?
+pub extern "C" fn str2rust(string: *const c_char) {
+    println!("{:?}", str_in(string));
 }
- 
 
+//viz. https://metacpan.org/pod/FFI::Platypus::Lang::Rust#returning-strings
+fn str_out(o_string: &str) -> *const u8 {
+    thread_local! {
+        static KEEP: RefCell<Option<CString>> = RefCell::new(None);
+    }
  
+    let c_string = CString::new(o_string).unwrap();
+    let ptr = c_string.as_ptr();
+    KEEP.with(|k| {
+        *k.borrow_mut() = Some(c_string);
+    });
+ 
+    ptr
+}
+#[no_mangle]
+pub extern "C" fn str2raku() -> *const u8 {
+    str_out("yo")
+}
+
 #[no_mangle]
 pub extern "C" fn add(a:i32, b:i32) -> i32 {
     a+b 

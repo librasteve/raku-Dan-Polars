87ca963973b9154b7a3675710b05be48cbfb2bf1
p6steve <p6steve@furnival.net>
2022-05-28 09:43:32 +0000

    ok ffi omnibus CPointer

--- spike4/dan/src/lib.rs
+++ spike4/dan/src/lib.rs
@@ -35,15 +35,80 @@ pub extern "C" fn df_read_csv(string: *const c_char) {
 }
 
 
-#[no_mangle]
-pub extern "C" fn df_ret_csv(string: *const c_char) -> *const DataFrame {
-    let df = df_load_csv(&str_in(string)).unwrap();
-    &df as *const _ 
+//#[no_mangle]
+//pub extern "C" fn df_ret_csv(string: *const c_char) -> *const DataFrame {
+//    let df = df_load_csv(&str_in(string)).unwrap();
+//    &df as *const _ 
+//}
+//#[no_mangle]
+//pub extern "C" fn df_head( df: DataFrame ) {
+////pub extern "C" fn df_head( df: <*const DataFrame> ) {
+//    println!{"{}", df.head(Some(2))};
+//}
+
+
+pub struct ZipCodeDatabase {
+    population: HashMap<String, u32>,
 }
+
+impl ZipCodeDatabase {
+    fn new() -> ZipCodeDatabase {
+        ZipCodeDatabase {
+            population: HashMap::new(),
+        }
+    }
+
+    fn populate(&mut self) {
+        for i in 0..100_000 {
+            let zip = format!("{:05}", i);
+            self.population.insert(zip, i);
+        }
+    }
+
+    fn population_of(&self, zip: &str) -> u32 {
+        self.population.get(zip).cloned().unwrap_or(0)
+    }
+}
+
 #[no_mangle]
-pub extern "C" fn df_head( df: DataFrame ) {
-//pub extern "C" fn df_head( df: <*const DataFrame> ) {
-    println!{"{}", df.head(Some(2))};
+pub extern "C" fn zip_code_database_new() -> *mut ZipCodeDatabase {
+    Box::into_raw(Box::new(ZipCodeDatabase::new()))
+}
+
+#[no_mangle]
+pub extern "C" fn zip_code_database_free(ptr: *mut ZipCodeDatabase) {
+    if ptr.is_null() {
+        return;
+    }
+    unsafe {
+        Box::from_raw(ptr);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn zip_code_database_populate(ptr: *mut ZipCodeDatabase) {
+    let database = unsafe {
+        assert!(!ptr.is_null());
+        &mut *ptr
+    };
+    database.populate();
+}
+
+#[no_mangle]
+pub extern "C" fn zip_code_database_population_of(
+    ptr: *const ZipCodeDatabase,
+    zip: *const c_char,
+) -> u32 {
+    let database = unsafe {
+        assert!(!ptr.is_null());
+        &*ptr
+    };
+    let zip = unsafe {
+        assert!(!zip.is_null());
+        CStr::from_ptr(zip)
+    };
+    let zip_str = zip.to_str().unwrap();
+    database.population_of(zip_str)
 }
 
 

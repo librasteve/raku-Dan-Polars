a71d521bbc59a9fd8df23ae5d534545e79c786cb
p6steve <p6steve@furnival.net>
2022-05-29 09:34:58 +0000

    bring in Inline::Rust (not Tuples)

--- dan/src/lib.rs
+++ dan/src/lib.rs
@@ -1,9 +1,13 @@
+use libc::c_char;
+use libc::size_t;
+use std::slice;
 use std::ffi::*; //{CStr, CString,}
+use std::iter;
 use std::collections::HashMap;
 use std::cell::RefCell;
 use std::fs::File;
 use std::path::{Path};
-use libc::c_char;
+
 
 use polars::prelude::*;//{CsvReader, DataType, Field, Schema, DataFrame,};
 use polars::prelude::{Result as PolarResult};
@@ -34,17 +38,104 @@ pub extern "C" fn df_read_csv(string: *const c_char) {
     println!{"{:?}", x};
 }
 
+fn str_in(i_string: *const c_char) -> String {
+    unsafe {
+        CStr::from_ptr(i_string).to_string_lossy().into_owned()
+    }
+}
+#[no_mangle]
+pub extern "C" fn str2rust(string: *const c_char) {
+    println!("{:?}", str_in(string));
+}
 
-//#[no_mangle]
-//pub extern "C" fn df_ret_csv(string: *const c_char) -> *const DataFrame {
-//    let df = df_load_csv(&str_in(string)).unwrap();
-//    &df as *const _ 
-//}
-//#[no_mangle]
-//pub extern "C" fn df_head( df: DataFrame ) {
-////pub extern "C" fn df_head( df: <*const DataFrame> ) {
-//    println!{"{}", df.head(Some(2))};
-//}
+//viz. https://metacpan.org/pod/FFI::Platypus::Lang::Rust#returning-strings
+fn str_out(o_string: &str) -> *const u8 {
+    thread_local! {
+        static KEEP: RefCell<Option<CString>> = RefCell::new(None);
+    }
+ 
+    let c_string = CString::new(o_string).unwrap();
+    let ptr = c_string.as_ptr();
+    KEEP.with(|k| {
+        *k.borrow_mut() = Some(c_string);
+    });
+ 
+    ptr
+}
+#[no_mangle]
+pub extern "C" fn str2raku() -> *const u8 {
+    str_out("yo")
+}
+
+#[no_mangle]
+pub extern "C" fn add(a:i32, b:i32) -> i32 {
+    a+b 
+}
+
+
+
+// Rust FFI Omnibus: Integers
+// http://jakegoulding.com/rust-ffi-omnibus/integers/
+
+#[no_mangle]
+pub extern "C" fn addition(a:i32, b:i32) -> i32 {
+    a+b
+}
+
+// Rust FFI Omnibus: String Arguments
+// http://jakegoulding.com/rust-ffi-omnibus/string-arguments/
+
+#[no_mangle]
+pub extern "C" fn how_many_characters(s: *const c_char) -> u32 {
+    let c_str = unsafe {
+        assert!(!s.is_null());
+
+        CStr::from_ptr(s)
+    };
+
+    let r_str = c_str.to_str().unwrap();
+    r_str.chars().count() as u32
+}
+
+// Rust FFI Omnibus: String Return Values
+// http://jakegoulding.com/rust-ffi-omnibus/string-return-values/
+
+#[no_mangle]
+pub extern "C" fn theme_song_generate(length: u8) -> *mut c_char {
+    let mut song = String::from("💣 ");
+    song.extend(iter::repeat("na ").take(length as usize));
+    song.push_str("Batman! 💣");
+
+    let c_str_song = CString::new(song).unwrap();
+    c_str_song.into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn theme_song_free(s: *mut c_char) {
+    unsafe {
+        if s.is_null() {
+            return;
+        }
+        CString::from_raw(s)
+    };
+}
+
+// Rust FFI Omnibus: Slice Arguments
+// http://jakegoulding.com/rust-ffi-omnibus/slice-arguments/
+
+#[no_mangle]
+pub extern "C" fn sum_of_even(n: *const u32, len: size_t) -> u32 {
+    let numbers = unsafe {
+        assert!(!n.is_null());
+
+        slice::from_raw_parts(n, len as usize)
+    };
+
+    numbers
+        .iter()
+        .filter(|&v| v % 2 == 0)
+        .sum()
+}
 
 
 pub struct ZipCodeDatabase {
@@ -111,37 +202,3 @@ pub extern "C" fn zip_code_database_population_of(
     database.population_of(zip_str)
 }
 
-
-fn str_in(i_string: *const c_char) -> String {
-    unsafe {
-        CStr::from_ptr(i_string).to_string_lossy().into_owned()
-    }
-}
-#[no_mangle]
-pub extern "C" fn str2rust(string: *const c_char) {
-    println!("{:?}", str_in(string));
-}
-
-//viz. https://metacpan.org/pod/FFI::Platypus::Lang::Rust#returning-strings
-fn str_out(o_string: &str) -> *const u8 {
-    thread_local! {
-        static KEEP: RefCell<Option<CString>> = RefCell::new(None);
-    }
- 
-    let c_string = CString::new(o_string).unwrap();
-    let ptr = c_string.as_ptr();
-    KEEP.with(|k| {
-        *k.borrow_mut() = Some(c_string);
-    });
- 
-    ptr
-}
-#[no_mangle]
-pub extern "C" fn str2raku() -> *const u8 {
-    str_out("yo")
-}
-
-#[no_mangle]
-pub extern "C" fn add(a:i32, b:i32) -> i32 {
-    a+b 
-}
--- lib/Dan/dan-rs.raku
+++ lib/Dan/dan-rs.raku
@@ -2,6 +2,9 @@ use NativeCall;
 
 constant $n-path = '../../dan/target/debug/dan';
 
+sub add (int32, int32) returns int32 is native($n-path) { * };  
+say add(1, 2); 
+
 sub str2rust(Str is encoded('utf8')) is native($n-path) {*};
 str2rust("hey");
 
@@ -11,14 +14,40 @@ say str2raku.deref;
 sub df_read_csv(Str is encoded('utf8')) is native($n-path) {*};
 df_read_csv("../../dan/src/iris.csv");
 
-#`[
-sub df_ret_csv(Str is encoded('utf8')) returns Pointer is native($n-path) {*};
-my $dfp = df_ret_csv("./dan/src/iris.csv");
-dd $dfp;
 
-sub df_head(Pointer) is native($n-path) {*};
-df_head($dfp);
-#]
+## Rust FFI Omnibus: Integers
+## http://jakegoulding.com/rust-ffi-omnibus/integers/
+
+sub addition(int32, int32) returns int32 is native($n-path) { * }
+
+say addition(1, 2);
+
+## Rust FFI Omnibus: String Arguments
+## http://jakegoulding.com/rust-ffi-omnibus/string-arguments/
+
+sub how_many_characters(Str is encoded('utf8')) returns int32 is native($n-path) { * }
+
+say how_many_characters("göes to élevên");
+
+## Rust FFI Omnibus: String Return Values
+## http://jakegoulding.com/rust-ffi-omnibus/string-return-values/
+
+sub theme_song_generate(uint8) returns Pointer[Str] is encoded('utf8') is native($n-path) { * }
+sub theme_song_free(Pointer[Str]) is native($n-path) { * }
+
+my \song = theme_song_generate(5);
+say song.deref;
+theme_song_free(song);
+
+## Rust FFI Omnibus: Slice Arguments
+## http://jakegoulding.com/rust-ffi-omnibus/slice-arguments/
+
+sub sum_of_even(CArray[uint32], size_t) returns uint32 is native($n-path) { * }
+
+my @numbers := CArray[uint32].new;
+@numbers[$++] = $_ for 1..6;
+
+say sum_of_even( @numbers, @numbers.elems );
 
 class ZipCodeDatabase is repr('CPointer') {
     sub zip_code_database_new() returns ZipCodeDatabase is native($n-path) { * }
@@ -52,6 +81,4 @@ my \pop2 = database.population_of('20500');
 say pop1 - pop2;
 
 
-sub add (int32, int32) returns int32 is native($n-path) { * };  
-say add(1, 2); 
 

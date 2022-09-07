ff51d660d7242d709a65917b0bb1e0473f127f9c
p6steve <40125330+p6steve@users.noreply.github.com>
2022-09-07 17:47:54 +0100

    Update Build.pm

--- Build.pm
+++ Build.pm
@@ -3,11 +3,11 @@ class Build {
         warn 'yo';
         say 'no', $dist-path;
         #say indir( 'dan', {run <cargo build>} );
-        run 'echo', 'Raku is Great!';
-        #`[
+        #run 'echo', 'Raku is Great!';
+        #[
         my $proc = run 'echo', 'Raku is Great!', :out, :err;
-        $proc.out.slurp(:close).say; # OUTPUT: «Raku is Great!␤» 
-        $proc.err.slurp(:close).say; # OUTPUT: «␤»
+        warn $proc.out.slurp(:close).say; # OUTPUT: «Raku is Great!␤» 
+        warn $proc.err.slurp(:close).say; # OUTPUT: «␤»
         #]
         # do build stuff to your module
         # which is located at $dist-path

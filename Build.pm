9d062bc478785fb2bd7c5ba03f074b4f8ea9dc8e
p6steve <40125330+p6steve@users.noreply.github.com>
2022-09-07 17:00:54 +0100

    Update Build.pm

--- Build.pm
+++ Build.pm
@@ -1,7 +1,7 @@
 class Build {
     method build($dist-path) {
-        say 'yo';
-        warn $dist-path;
+        warn 'yo';
+        die 'no', $dist-path;
         # do build stuff to your module
         # which is located at $dist-path
     }

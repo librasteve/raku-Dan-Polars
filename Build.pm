056006f6f793c254da1b87a056e33edfa06b5f62
p6steve <40125330+p6steve@users.noreply.github.com>
2022-09-07 17:11:41 +0100

    Update Build.pm

--- Build.pm
+++ Build.pm
@@ -2,7 +2,8 @@ class Build {
     method build($dist-path) {
         warn 'yo';
         warn 'no', $dist-path;
-        warn indir( 'dan', {run <cargo build>} );
+        #warn indir( 'dan', {run <cargo build>} );
+        warn indir( 'dan', {shell 'cargo build'} );
         # do build stuff to your module
         # which is located at $dist-path
     }

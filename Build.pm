49500187cebe2cebadd5abbd8ed046dfe823499c
p6steve <40125330+p6steve@users.noreply.github.com>
2022-09-07 17:49:13 +0100

    Update Build.pm

--- Build.pm
+++ Build.pm
@@ -6,8 +6,8 @@ class Build {
         #run 'echo', 'Raku is Great!';
         #[
         my $proc = run 'echo', 'Raku is Great!', :out, :err;
-        warn $proc.out.slurp(:close).say; # OUTPUT: «Raku is Great!␤» 
-        warn $proc.err.slurp(:close).say; # OUTPUT: «␤»
+        say $proc.out.slurp(:close).say; # OUTPUT: «Raku is Great!␤» 
+        say $proc.err.slurp(:close).say; # OUTPUT: «␤»
         #]
         # do build stuff to your module
         # which is located at $dist-path

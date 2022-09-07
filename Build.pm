68080bac25731e23f37d98d597b27d7ac00118d1
p6steve <40125330+p6steve@users.noreply.github.com>
2022-09-07 18:31:29 +0100

    add >

--- Build.pm
+++ Build.pm
@@ -1,6 +1,6 @@
 class Build {
     method build($dist-path) {
-        warn '> Running cargo build against Polars Cargo.toml (may take a few mins).';
+        warn '>Running cargo build against Polars Cargo.toml (may take a few mins).';
         chdir 'dan';
           
         my $proc = Proc::Async.new: run <cargo build>;

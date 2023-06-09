9d93a394ed1b1ca01472f1ecafae7638bfaadd1d
librasteve <librasteve@furnival.net>
2023-06-09 15:47:55 +0000

    hardwore libadder.so

--- spike5/src/main.rs
+++ spike5/src/main.rs
@@ -1,16 +1,12 @@
 extern crate libloading;
 
-use std::env;
 use libloading::{Library, Symbol};
 
 type AddFunc = fn(isize, isize) -> isize;
 
 fn main() {
-    let library_path = env::args().nth(1).expect("USAGE: loading <LIB>");
-    println!("Loading addd() from {}", library_path);
-
     unsafe {
-        let lib = Library::new(library_path).unwrap();
+        let lib = Library::new("./libadder.so").unwrap();
 
         let func: Symbol<AddFunc> = lib.get(b"add").unwrap();
 

74faa228308f82d6871acd987a51fcd30f77cb76
librasteve <40125330+librasteve@users.noreply.github.com>
2023-08-18 10:48:09 +0100

    cargo build in resources

--- Build.pm
+++ Build.pm
@@ -1,23 +1,24 @@
 class Build {
     method build($dist-path) {
+        mkdir 'resources';
+        mkdir 'resources/libraries';
+        move 'dan/src/*', 'resources/rust/src';
         
-        chdir 'dan';        
+        chdir 'resources/rust/src';        
         warn ' Building Rust Polars library (may take a few minutes).';
         my $proc = Proc::Async.new: <cargo build>;
         $proc.bind-stdout($*ERR);
         my $promise = $proc.start;
         await $promise;
         
-        chdir '..';
-        mkdir 'resources';
-        
-        mkdir 'resources/libraries';
-        move 'dan/target/debug/libdan.so', 'resources/libraries/libdan.so';
 
+
+        #`[
         mkdir 'resources/apply';
         mkdir 'resources/apply/src';
         move 'dan/src/apply-template.rs', 'resources/apply/src/apply-template.rs';
         move 'dan/target/debug/deps/*',   'resources/apply/target/debug/deps/';
+        #]
         
         warn 'Build successful';
         

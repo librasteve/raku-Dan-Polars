a35ecd8ef23040593c20c2bce3da1b904a13bc84
librasteve <40125330+librasteve@users.noreply.github.com>
2023-08-19 12:10:05 +0100

    move then build

--- Build.pm
+++ Build.pm
@@ -8,8 +8,28 @@ class Build {
         move 'dan/src/apply-template.rs', 'resources/dan/src/apply-template.rs';
         #]
 
-        #[ original - works
-        chdir 'dan';        
+        mkdir 'resources';
+        mkdir 'resources/dan';
+        mkdir 'resources/dan/src';
+        mkdir 'resources/dan/target';
+        mkdir 'resources/dan/target/debug';
+        move 'dan/Cargo.toml', 'resources/dan/Cargo.toml';
+        move 'dan/src/lib.rs', 'resources/dan/src/lib.rs';
+        move 'dan/src/apply-template.rs', 'resources/dan/src/apply-template.rs';
+
+        chdir 'resources/dan';        
+        warn ' Building Rust Polars library (may take a few minutes).';
+        my $proc = Proc::Async.new: <cargo build>;
+        $proc.bind-stdout($*ERR);
+        my $promise = $proc.start;
+        await $promise;
+
+        chdir '../..';
+        mkdir 'resources/libraries';
+        move 'resources/dan/target/debug/libdan.so', 'resources/libraries/libdan.so';
+
+        #`[ original - works
+        chdir 'dan';
         warn ' Building Rust Polars library (may take a few minutes).';
         my $proc = Proc::Async.new: <cargo build>;
         $proc.bind-stdout($*ERR);

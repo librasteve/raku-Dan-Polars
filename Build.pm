48db8b54d81a83096dca59747a5856971cbf53d9
librasteve <40125330+librasteve@users.noreply.github.com>
2023-08-22 11:54:44 +0100

    wip

--- Build.pm
+++ Build.pm
@@ -1,20 +1,6 @@
 class Build {
     method build($dist-path) {
-        #`[ original - works
-        chdir 'dan';
-        warn ' Building Rust Polars library (may take a few minutes).';
-        my $proc = Proc::Async.new: <cargo build>;
-        $proc.bind-stdout($*ERR);
-        my $promise = $proc.start;
-        await $promise;
-        
-        chdir '..';
-        mkdir 'resources';
-        mkdir 'resources/libraries';
-        move 'dan/target/debug/libdan.so', 'resources/libraries/libdan.so';
-        #]
-
-        #[ BUILD IN RESOURCES
+        #[ CARGO BUILD IN RESOURCES
         mkdir 'resources';
         mkdir 'resources/dan';
         mkdir 'resources/dan/src';
@@ -38,7 +24,14 @@ class Build {
         mkdir 'resources/libraries';
         move 'resources/dan/target/debug/libdan.so', 'resources/libraries/libdan.so';
         warn qqx`cd resources/libraries && ls -al`;
-        #warn qqx`tree`;
+        #]
+
+        #`[
+        chdir 'resources';
+        warn qqx`tree`;
+        my $cu = CompUnit::Repository::FileSystem.new(prefix => $*CWD.add("dan"));
+        warn (dd $cu);
+        warn $cu.distribution.meta<resources>;
         #]
         
         warn 'Build successful';
@@ -61,7 +54,7 @@ class Build {
         move 'dan/target/debug/libdan.so', 'resources/libraries/libdan.so';
         #]
 
-        #`[ BUILD IN RESOURCES
+        #`[ CARGO BUILD IN RESOURCES
         mkdir 'resources';
         mkdir 'resources/dan';
         mkdir 'resources/dan/src';

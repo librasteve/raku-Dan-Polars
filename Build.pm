2cb89db5f5fb17338f89bf2a59f8c33a7960a2bd
librasteve <40125330+librasteve@users.noreply.github.com>
2023-08-22 11:21:44 +0100

    wip

--- Build.pm
+++ Build.pm
@@ -1,39 +1,6 @@
 class Build {
     method build($dist-path) {
-        #new partial - cargo build phase to script
-        #`[
-        mkdir 'resources/dan';
-        mkdir 'resources/dan/src';
-        move 'dan/Cargo.toml', 'resources/dan/Cargo.toml';
-        move 'dan/src/apply-template.rs', 'resources/dan/src/apply-template.rs';
-        #]
-
-        mkdir 'resources';
-        mkdir 'resources/dan';
-        mkdir 'resources/dan/src';
-        mkdir 'resources/dan/target';
-        mkdir 'resources/dan/target/debug';
-        mkdir 'resources/dan/target/debug/deps';
-        move 'dan/Cargo.toml', 'resources/dan/Cargo.toml';
-        move 'dan/src/lib.rs', 'resources/dan/src/lib.rs';
-        move 'dan/src/apply-template.rs', 'resources/dan/src/apply-template.rs';
-
-        chdir 'resources/dan';        
-        warn ' Building Rust Polars library (may take a few minutes).';
-        my $proc = Proc::Async.new: <cargo build>;
-        $proc.bind-stdout($*ERR);
-        my $promise = $proc.start;
-        await $promise;
-
-#[
-        chdir '../..';
-        mkdir 'resources/libraries';
-        move 'resources/dan/target/debug/libdan.so', 'resources/libraries/libdan.so';
-        warn qqx`cd resources/libraries && ls -al`;
-        #warn qqx`tree`;
-#]
-
-        #`[ original - works
+        #[ original - works
         chdir 'dan';
         warn ' Building Rust Polars library (may take a few minutes).';
         my $proc = Proc::Async.new: <cargo build>;
@@ -52,3 +19,44 @@ class Build {
         exit 0
     }
 }
+
+        #`[ original - works
+        chdir 'dan';
+        warn ' Building Rust Polars library (may take a few minutes).';
+        my $proc = Proc::Async.new: <cargo build>;
+        $proc.bind-stdout($*ERR);
+        my $promise = $proc.start;
+        await $promise;
+        
+        chdir '..';
+        mkdir 'resources';
+        mkdir 'resources/libraries';
+        move 'dan/target/debug/libdan.so', 'resources/libraries/libdan.so';
+        #]
+
+        #`[ BUILD IN RESOURCES
+        mkdir 'resources';
+        mkdir 'resources/dan';
+        mkdir 'resources/dan/src';
+        mkdir 'resources/dan/target';
+        mkdir 'resources/dan/target/debug';
+        mkdir 'resources/dan/target/debug/deps';
+        move 'dan/Cargo.toml', 'resources/dan/Cargo.toml';
+        move 'dan/src/lib.rs', 'resources/dan/src/lib.rs';
+        move 'dan/src/apply-template.rs', 'resources/dan/src/apply-template.rs';
+
+        chdir 'resources/dan';        
+        warn ' Building Rust Polars library (may take a few minutes).';
+        my $proc = Proc::Async.new: <cargo build>;
+        $proc.bind-stdout($*ERR);
+        my $promise = $proc.start;
+        await $promise;
+        #]
+
+        #`[ THEN REPOSITION LIBDAN.SO
+        chdir '../..';
+        mkdir 'resources/libraries';
+        move 'resources/dan/target/debug/libdan.so', 'resources/libraries/libdan.so';
+        warn qqx`cd resources/libraries && ls -al`;
+        #warn qqx`tree`;
+        #]

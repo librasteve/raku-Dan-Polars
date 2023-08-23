75189a1f8a33d715ab72b53bb5129b6a39569471
librasteve <40125330+librasteve@users.noreply.github.com>
2023-08-23 08:51:39 +0100

    re-add orig

--- Build.pm
+++ Build.pm
@@ -1,5 +1,17 @@
 class Build {
     method build($dist-path) {
+        chdir 'dan';
+        warn ' Building Rust Polars library (may take a few minutes).';
+        my $proc = Proc::Async.new: <cargo build>;
+        $proc.bind-stdout($*ERR);
+        my $promise = $proc.start;
+        await $promise;
+        
+        chdir '..';
+        mkdir 'resources';
+        mkdir 'resources/libraries';
+        move 'dan/target/debug/libdan.so', 'resources/libraries/libdan.so';
+        
         mkdir 'resources';
         mkdir 'resources/dan';
         mkdir 'resources/dan/src';
